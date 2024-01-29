a2359954ebe8b1797320a98c7517319ee7ba31b5
MaelRibes <90307031+MaelRibes@users.noreply.github.com>
2024-01-29 22:44:48 +0200

    Replicat set patch

--- scripts/init-replica-set.sh
+++ scripts/init-replica-set.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
+echo "Initialisation du Replica Set MongoDB..."
 
-mongosh --host mongodb_primary:27017 <<EOF
-var config = {
-    "_id": "rs0",
-    "version": 1,
-    "members": [
-        {
-            "_id": 0,
-            "host": "mongodb_primary:27017",
-            "priority": 3
-        },
-        {
-            "_id": 1,
-            "host": "mongodb_secondary1:27017",
-            "priority": 2
-        },
-        {
-            "_id": 2,
-            "host": "mongodb_secondary2:27017",
-            "priority": 1
-        }
+# La commande mongosh pour initialiser le replica set
+mongosh --eval "
+rs.initiate({
+    _id : 'rs0',
+    members: [
+        { _id : 0, host : 'mongodb_primary:27017' },
+        { _id : 1, host : 'mongodb_secondary1:27017' },
+        { _id : 2, host : 'mongodb_secondary2:27017' }
     ]
-};
-rs.initiate(config, { force: true });
-rs.status();
-EOF
+});
+"
+echo "Replica Set initialisé."
--- start.sh
+++ start.sh
@@ -2,11 +2,4 @@
 
 docker-compose up -d
 
-echo "Waiting for MongoDB to be ready..."
-while ! docker exec mongodb_primary mongosh --host mongodb_primary:27017 --eval "db.stats()" &>/dev/null; do
-    sleep 5
-    echo "Waiting for MongoDB..."
-done
-
-echo "MongoDB is ready. Initializing the replica set."
 docker exec mongodb_primary /scripts/init-replica-set.sh
