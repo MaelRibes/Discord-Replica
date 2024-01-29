88aa6a1aaf72751ccd8ade146041e0f409836c37
MaelRibes <90307031+MaelRibes@users.noreply.github.com>
2024-01-29 22:54:34 +0200

    Replicat set patch

--- scripts/init-replica-set.sh
+++ scripts/init-replica-set.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "Initialisation du Replica Set MongoDB..."
+echo "Initialization of the MongoDB Replica Set ..."
 
 # La commande mongosh pour initialiser le replica set
 mongosh --eval "
 rs.initiate({
     _id : 'rs0',
     members: [
-        { _id : 0, host : 'mongodb_primary:27017' },
-        { _id : 1, host : 'mongodb_secondary1:27017' },
-        { _id : 2, host : 'mongodb_secondary2:27017' }
+        { _id : 0, host : 'mongodb_primary:27017', priority: 3},
+        { _id : 1, host : 'mongodb_secondary1:27017', priority: 2 },
+        { _id : 2, host : 'mongodb_secondary2:27017', priority: 1 }
     ]
 });
 "
-echo "Replica Set initialisé."
+echo "Replica Set initializes."
